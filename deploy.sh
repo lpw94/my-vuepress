0d5d49c6e654c314c7b6c4517dc7502353ee4a6b
lupinwo <43490@sangfor.com>
2024-01-08 15:34:58 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,4 @@
-# 确保脚本抛出遇到的错误
-set -e
-
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 若是是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
-git init
+git init			
 git add -add
-git commit -m 'master'
-
-# 若是发布到 https://<USERNAME>.github.io
-git push -f https://github.com/lpw94/lpw94.github.io master:gh-pages
-
-# 若是发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-cd -
+git commit -m 'deploy'
+git push -f git@github.com:lpw94/lpw94.github.io.git master
