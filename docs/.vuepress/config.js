33d70d5e8e3f2a0b6f05e6e31419e52399dcc8ec
lupinwo <43490@sangfor.com>
2024-01-10 15:05:52 +0800

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,5 +5,24 @@ module.exports = {
     description: 'ES6, JavaScript, typyscript, vue2, vue3, react, HTML技术学习。',
     themeConfig: {
         nav: [{ text: '个人简历', link: '/blog/' }]
-    }
+    },
+    plugins: [
+        // 彩带背景
+        [
+          'ribbon',
+          {
+            size: 90,
+            opacity: 0.8,
+            zIndex: -1,
+          },
+        ],
+        // 鼠标特效插件
+        [
+          'cursor-effects',
+          {
+            size: 1.75,
+            shape: 'star',
+          },
+        ],
+      ],
 }
